fc0df467c572b9f926ed1eb4ad5b483db1659d9a
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-11-10 15:42:12 +0100

    j ai fini le partie de faire une fonction qui ouvrir les questions et je vais change le fonction qui ouvrir le categorie des questions

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,30 @@
 function UserRights() {
     const user_rights = document.getElementById('UserRights');
-    user_rights.classList.remove('hidden');
-    
+    user_rights.classList.toggle('hidden');
+}
+let i;
+function ouvrir_qustion(i) {
+switch (i) {
+    case 1:
+     document.getElementById('Q1').classList.toggle('hidden');
+        break;
+    case 2:
+     document.getElementById('Q2').classList.toggle('hidden');
+        break;
+    case 3:
+     document.getElementById('Q3').classList.toggle('hidden');
+        break;
+    case 4:
+     document.getElementById('Q4').classList.toggle('hidden');
+        break;
+    case 5:
+     document.getElementById('Q5').classList.toggle('hidden');
+        break;
+    case 6:
+    document.getElementById('Q6').classList.toggle('hidden');
+        break;
+    default:
+        break;
+}
+
 }
