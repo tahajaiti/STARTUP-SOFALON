9ab5f4a70100183045544eaf540595f75547f888
Nada Zirari <zirarinada20@gmail.com>
2024-11-11 14:50:09 +0100

    animations pendant le scroll

--- public/assets/about.js/script.js
+++ public/assets/about.js/script.js
@@ -1,23 +1,51 @@
-// Enregistre le plugin ScrollTrigger avec GSAP
+
 gsap.registerPlugin(ScrollTrigger);
 
-// Sélectionne toutes les images dans les sections pour l'animation au scroll
-const images = document.querySelectorAll("main img");
 
-// Applique une animation à chaque image avec ScrollTrigger
-images.forEach((image) => {
+const divs = document.querySelectorAll("main div");
+
+const imgs = document.querySelectorAll("main img");
+
+
+divs.forEach((div, index) => {
+ 
+  const fromX = index % 2 === 0 ? 200 : -200; 
+
   gsap.fromTo(
-    image,
-    { x: -200, opacity: 0 }, // état de départ : hors de l'écran à gauche et invisible
+    div,
+    { x: fromX, opacity: 0 }, 
     {
       x: 0,
       opacity: 1,
       duration: 1.5,
       ease: "power2.out",
       scrollTrigger: {
-        trigger: image, // déclenche l'animation lorsque l'image entre dans la vue
-        start: "top 80%", // commence l'animation quand le haut de l'image atteint 80% de la hauteur de la fenêtre
-        toggleActions: "play none none none", // lance l'animation et ne la rejoue pas quand on quitte la zone de vue
+        trigger: div, 
+        start: "top 80%", 
+        toggleActions: "play none none none",
+      },
+    }
+  );
+});
+
+
+
+imgs.forEach((img, index) => {
+  
+  const fromX = index % 2 === 0 ? 200 : -200; 
+
+  gsap.fromTo(
+    img,
+    { x: fromX, opacity: 0 }, 
+    {
+      x: 0,
+      opacity: 1,
+      duration: 1.5,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: img, 
+        start: "top 80%", 
+        toggleActions: "play none none none", 
       },
     }
   );
