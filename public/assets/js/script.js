e56724626e4235a0f4167f49a5d9ac3007ebb700
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-11-11 12:42:09 +0100

    add a function for a paginition dynamic

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,8 @@
 let card = document.getElementById('card');
+const pagination = document.getElementById('pagination');
 let pagination1 = document.getElementById('pagination1');
 let pagination2 = document.getElementById('pagination2');
+
 
 const sortButton = document.getElementById('sortButton');
 const filterButton = document.getElementById('filterButton');
@@ -8,64 +10,91 @@ const sortOptions = document.getElementById('sortOptions');
 const filterOptions = document.getElementById('filterOptions');
 
 let data = null;
+let InitialProducts;
 
 
+let currentPageN;
 async function fetchProducts() {
-    const response = await fetch('../../../products.json'); 
+    const response = await fetch('../../../products.json');
     data = await response.json();
-    DisplayCards(data.products[0].Beds, data.products[0].Decoration, data.products[0].Kitchen);
+
+    currentPageN = Math.ceil(data.products.length / 12);
+    console.log(currentPageN);
+
+    DisplayCards(GFG(data.products, 1, 12));
+    addPagination();
 }
 
 
-function DisplayCards(...categories) {
-    card.innerHTML = ''; 
-    categories.forEach(category => {
-        category.forEach(product => {
-            card.innerHTML += `
-                <div class="flex flex-col bg-white rounded-lg p-4 hover:shadow-lg transition-shadow duration-300 max-w-xs mx-auto">
-                    <img src="${product.mainImage}" class="bg-whiteGrey p-2" alt="Product Image">
-                    <div class="flex flex-col mt-4">
-                        <div class="reviews flex gap-1 items-center">
-                            <div class="flex">
-                                <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
-                                <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
-                                <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
-                                <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
-                                <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
-                            </div>
-                            <p class="text-SoftGray text-sm">(Reviews)</p>
-                            <p class="text-lBrown text-sm ml-auto">${product.price}</p>
+function GFG(array, currentPage, pageSize) {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return array.slice(startIndex, endIndex);
+}
+
+
+function DisplayCards(products) {
+    card.innerHTML = '';
+    products.forEach(product => {
+        card.innerHTML += `
+            <div class="flex flex-col bg-white rounded-lg p-4 hover:shadow-lg transition-shadow duration-300 max-w-xs mx-auto">
+                <img src="${product.mainImage}" class="bg-whiteGrey p-2" alt="Product Image">
+                <div class="flex flex-col mt-4">
+                    <div class="reviews flex gap-1 items-center">
+                        <div class="flex">
+                            <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
+                            <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
+                            <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
+                            <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
+                            <span class="icon-[ri--star-fill] text-lBrown w-3"></span>
                         </div>
-                        <p class="text-lg font-semibold mt-2">${product.title}</p>
-                        <button class="bg-lBrown text-white py-1 rounded mt-2 border-2 border-transparent hover:bg-white hover:border-lBrown hover:text-lBrown transition-all duration-300">
-                            SEE DETAILS
-                        </button>
+                        <p class="text-SoftGray text-sm">(Reviews)</p>
+                        <p class="text-lBrown text-sm ml-auto">${product.price}</p>
                     </div>
-                </div>`;
-        });
+                    <p class="text-lg font-semibold mt-2">${product.title}</p>
+                    <button class="bg-lBrown text-white py-1 rounded mt-2 border-2 border-transparent hover:bg-white hover:border-lBrown hover:text-lBrown transition-all duration-300">
+                        SEE DETAILS
+                    </button>
+                </div>
+            </div>`;
     });
 }
 
+const addPagination = () => {
+    if (currentPageN > 0) {
+        for (let i = 1; i <= currentPageN; i++) {
+            const newPage = document.createElement('div');
+            newPage.className = "flex w-8 h-8 border border-SoftGray text-lBrown text-xl justify-center items-center hover:cursor-pointer";
+            newPage.id = `page`;
+            newPage.textContent = i;
+            pagination.appendChild(newPage);
+            newPage.addEventListener('click', () => {
+                DisplayCards(GFG(data.products, i, 12));
 
-pagination1.addEventListener('click', () => {
-    setActiveButton(pagination1, pagination2);
-    DisplayCards(data.products[0].Beds, data.products[0].Decoration, data.products[0].Kitchen);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+                console.log(GFG(data.products, i, 12));
 
-pagination2.addEventListener('click', () => {
-    setActiveButton(pagination2, pagination1);
-    DisplayCards(data.products[0].Storage, data.products[0].Tables);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+                ActivePage(newPage);
+            });
+        };
+        const firstPage = document.querySelector("#page");
+        ActivePage(firstPage);
+    };
+};
 
 
-function setActiveButton(activeButton, inactiveButton) {
-    activeButton.classList.add('bg-lBrown', 'text-white');
-    activeButton.classList.remove('bg-white', 'border', 'border-SoftGray', 'text-gray');
-    inactiveButton.classList.add('bg-white', 'border', 'border-SoftGray', 'text-gray');
-    inactiveButton.classList.remove('bg-lBrown', 'text-white');
+
+function ActivePage(activebtn) {
+    const AllPage = document.querySelectorAll('#page');
+
+    AllPage.forEach((page) => {
+        page.classList.add('bg-white', 'border', 'border-SoftGray', 'text-gray');
+        page.classList.remove('bg-lBrown', 'text-white');
+    });
+    activebtn.classList.add('bg-lBrown', 'text-white');
+    activebtn.classList.remove('bg-white', 'border', 'border-SoftGray', 'text-gray');
 }
+
 
 fetchProducts();
 
@@ -80,3 +109,10 @@ sortButton.addEventListener('click', () => {
     }
 });
 
+filterButton.addEventListener('click', () => {
+    if (filterOptions.style.display === 'none') {
+        filterOptions.style.display = 'block';
+    } else {
+        filterOptions.style.display = 'none';
+    }
+});
