2831aab92f905062b4f3931f1af8a7a9ca40eb1b
Taha <kyojin.rar@gmail.com>
2024-11-11 23:14:15 +0100

    made the products animated and gave them a random index so the same item wont repeat twice

--- public/assets/js/home/handler.js
+++ public/assets/js/home/handler.js
@@ -6,27 +6,57 @@ async function getData() {
         const response = await fetch('./products.json');
         data = await response.json();
 
-        console.log(data);
-        loadData(); // Call loadData after data is fetched
+        loadData();
     } catch (error) {
         console.error("Error fetching data:", error);
+    }
+}
+// shuffle
+const shuffleArray = (array) =>{
+    for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
 const loadData = () => {
     if (!data) return;
 
-    cards.forEach((card) => {
-        const randomIndex = Math.floor(Math.random() * data.products.length);
-        const randomProduct = data.products[randomIndex];
+    const dataCopy = [...data.products];
+    shuffleArray(dataCopy);
+
+    cards.forEach((card, index) => {
+        const random = dataCopy[index % dataCopy.length];
 
         const title = card.querySelector('#productName');
         const img = card.querySelector('img');
+        const price = card.querySelector('#productPrice');
 
-        if (title) title.textContent = randomProduct.title;
-        if (img) img.src = randomProduct.mainImage;
+        if (title) title.textContent = random.title;
+        if (img) img.src = random.mainImage;
+        if (price) price.textContent = `$${random.price}`;
     });
+
+    const timeline = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 500,
+    });
+
+    timeline
+        .add({
+            targets: cards,
+            translateX: [-10000, 0],
+            opacity: [0, 1],
+            scale: [0.3, 0.75],
+            filter: ["blur(10px)", "blur(0px)"],
+            delay: anime.stagger(100, {easing: 'easeOutElastic',direction: 'reverse'}),
+            duration: 700,
+            easing: 'easeInQuad',
+        });
 };
 
+setInterval  (() => {
+    loadData();
+}, 10000);
 
 document.addEventListener('DOMContentLoaded', getData);
