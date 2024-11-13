e97b35e4000aab02d27e883f39dff9d77324eed4
Nada Zirari <zirarinada20@gmail.com>
2024-11-13 10:22:51 +0100

    Modification concernant le container des items avec css et couleurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,20 @@ const { addDynamicIconSelectors } = require("@iconify/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         dark: "#232020",
-        gray: "#363537",
+        whittt: "#F6F6F6",
         mustard: "#FEFAE0",
         lBrown: "#DDA15E",
         dBrown: "#AA6122",
+        SoftGray: '#878787',
+        whiteGrey: '#F6F6F6',
       },
-      fontFamily: {
-        Mont: ["Montserrat", "sans-serif"], 
-        
-      },
+      fontFamily: { Mont: ["Montserrat", "sans-serif"] },
     },
   },
-  plugins: [
-    addDynamicIconSelectors(), 
-  ],
+  plugins: [addDynamicIconSelectors()],
 };
