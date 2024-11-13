2c848eba96d83795a9a3b2e62d080d919cf08397
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-11-13 17:29:24 +0100

    cree la fonction de sumbit

---
+++ public/assets/js/script.js
@@ -0,0 +1,15 @@
+let infor = [];
+
+
+const send = document.getElementById('send');
+ 
+send.addEventListener("click", (e) => {
+    e.preventDefault();
+    let obj = {
+        name : '',
+        email : '',
+        message : '',
+    }
+    
+});
+
