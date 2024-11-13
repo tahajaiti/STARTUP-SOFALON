46fe219ebb8011311b3daefe588a850ee3aec5ee
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-11-13 12:33:12 +0100

    faire le media querry sur les questions  et modfier en js et ajouter toggle pour afficher les questions de partie trust et payment

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,18 @@ function UserRights() {
     const user_rights = document.getElementById('UserRights');
     user_rights.classList.toggle('hidden');
 }
+function REFUNDPOLICY() {
+    const REFUNDPOLICY = document.getElementById('REFUNDPOLICY');
+    REFUNDPOLICY.classList.toggle('hidden');
+}
+function Trust() {
+    const Trust = document.getElementById('Trust');
+    Trust.classList.toggle('hidden');
+}
+function PAYMENT() {
+    const pay = document.getElementById('pay');
+    pay.classList.toggle('hidden');
+}
 let i;
 function ouvrir_qustion(i) {
 switch (i) {
@@ -22,6 +34,82 @@ switch (i) {
         break;
     case 6:
     document.getElementById('Q6').classList.toggle('hidden');
+        break;
+    default:
+        break;
+}
+
+}
+function ouvrir_qustion_box(i) {
+    
+switch (i) {
+    case 1:
+     document.getElementById('Q1_box').classList.toggle('hidden');
+        break;
+    case 2:
+     document.getElementById('Q2_box').classList.toggle('hidden');
+        break;
+    case 3:
+     document.getElementById('Q3_box').classList.toggle('hidden');
+        break;
+    case 4:
+     document.getElementById('Q4_box').classList.toggle('hidden');
+        break;
+    case 5:
+     document.getElementById('Q5_box').classList.toggle('hidden');
+        break;
+    case 6:
+    document.getElementById('Q6_box').classList.toggle('hidden');
+        break;
+    default:
+        break;
+}
+
+}
+function ouvrir_qustion_Trust(i) {
+switch (i) {
+    case 1:
+     document.getElementById('Q1_Trust').classList.toggle('hidden');
+        break;
+    case 2:
+     document.getElementById('Q2_Trust').classList.toggle('hidden');
+        break;
+    case 3:
+     document.getElementById('Q3_Trust').classList.toggle('hidden');
+        break;
+    case 4:
+     document.getElementById('Q4_Trust').classList.toggle('hidden');
+        break;
+    case 5:
+     document.getElementById('Q5_Trust').classList.toggle('hidden');
+        break;
+    case 6:
+    document.getElementById('Q6_Trust').classList.toggle('hidden');
+        break;
+    default:
+        break;
+}
+
+ }
+function ouvrir_qustion_pay(i) {
+switch (i) {
+    case 1:
+     document.getElementById('Q1_pay').classList.toggle('hidden');
+        break;
+    case 2:
+     document.getElementById('Q2_pay').classList.toggle('hidden');
+        break;
+    case 3:
+     document.getElementById('Q3_pay').classList.toggle('hidden');
+        break;
+    case 4:
+     document.getElementById('Q4_pay').classList.toggle('hidden');
+        break;
+    case 5:
+     document.getElementById('Q5_pay').classList.toggle('hidden');
+        break;
+    case 6:
+    document.getElementById('Q6_pay').classList.toggle('hidden');
         break;
     default:
         break;
