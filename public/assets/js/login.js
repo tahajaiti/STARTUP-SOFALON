ecc2177d6b0a4b2b1e028b75510aa05b90b47d92
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-11-17 16:32:19 +0100

    display an error message below the inputs instead of using an alert

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,28 +1,33 @@
 let login;
 
-  async function FetchLogin() {
-    const response = await fetch('../../../../login.json');
-    login = await response.json();
-    console.log("Fetched data:", login);
+async function FetchLogin() {
+  const response = await fetch('../../../../login.json');
+  login = await response.json();
+  console.log("Fetched data:", login);
+}
+
+const form = document.getElementById('form');
+const errorMsg = document.getElementById('errormsg');
+
+form.addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const emailValue = document.getElementById('email').value;
+  const passValue = document.getElementById('password').value;
+
+  console.log('Email:', emailValue, 'Password:', passValue);
+
+
+  errorMsg.classList.add('hidden');
+
+  if (emailValue === login.email && passValue === login.password) {
+    localStorage.setItem('login', 'true'); 
+    window.location.href = 'dashboard.html';
+  } else {
+
+    errorMsg.classList.remove('hidden');
+    errorMsg.textContent = 'Invalid email or password.';
   }
+});
 
-
-  const form = document.getElementById('form');
-  form.addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const emailValue = document.getElementById('email').value;
-    const passValue = document.getElementById('password').value;
-
-    console.log('Email:', emailValue, 'Password:', passValue);
-
-    if (emailValue === login.email && passValue === login.password) {
-      localStorage.setItem('login', 'true'); 
-      window.location.href = 'dashboard.html';
-    } else {
-      alert('Invalid email or password.');
-    }
-  });
-
-
-  FetchLogin();
+FetchLogin();
